4030bca48bdc4a3886f336b7912ecfdbfb044637
Taliesin Sisson <taliesins@yahoo.com>
2013-12-05 17:47:24 +0000

    Start adding certificate settings

--- src/OnPremise/WebSite/Deployment/install.ps1
+++ src/OnPremise/WebSite/Deployment/install.ps1
@@ -1,6 +1,6 @@
 ﻿[CmdletBinding()]
 param(
-	[string] $environmentConfigurationFilePath = (Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) "deployment_configuration.xml" ),
+	[string] $environmentConfigurationFilePath = (Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) "deployment_configuration.json" ),
 	[string] $productConfigurationFilePath = (Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) "configuration.xml" )
 )
 
@@ -10,8 +10,18 @@ Import-Module $scriptPath\PowershellModules\CommonDeploy.psm1 -Force
 $rootPath = Split-Path -parent $scriptPath
 
 #Ensure that certificate has been extracted from config
+
+$httpsCertificate = $environmentConfigurationFilePath.Certificates | ?{$_.Name == "HttpsCertificate"} | %{$_.Certificate}
+$httpsCertificatePassword = $environmentConfigurationFilePath.Certificates | ?{$_.Name == "HttpsCertificate"} | %{$_.Password}
+$tokenCertificate = $environmentConfigurationFilePath.Certificates | ?{$_.Name == "TokenCertificate"} | %{$_.Certificate}
+$tokenCertificatePassword = $environmentConfigurationFilePath.Certificates | ?{$_.Name == "TokenCertificate"} | %{$_.Password}
+
 #Install certificate if required
+
 #Add user permissions to certificate
+
+# security access needs to be unlocked
+%windir%\system32\inetsrv\appcmd.exe unlock -section:system.webServer/security/access
 
 # Setup the configuration
 $updateConfiguration = Join-Path $scriptPath "UpdateConfiguration.ps1"
